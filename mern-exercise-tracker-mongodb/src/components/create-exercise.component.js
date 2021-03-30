563b24cc0f6e2c1b3f6b1d742be7173e8ef6f49a
mb16biswas <64213233+mb16biswas@users.noreply.github.com>
2021-03-30 12:02:55 +0530

    Update create-exercise.component.js

--- mern-exercise-tracker-mongodb/src/components/create-exercise.component.js
+++ mern-exercise-tracker-mongodb/src/components/create-exercise.component.js
@@ -23,7 +23,7 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/users/')
+    axios.get('https://gym-deploy-safe.herokuapp.com/users/')
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
@@ -74,7 +74,7 @@ export default class CreateExercise extends Component {
 
     console.log(exercise);
 
-    axios.post('http://localhost:5000/exercises/add', exercise)
+    axios.post('https://gym-deploy-safe.herokuapp.com/exercises/add', exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
@@ -114,7 +114,7 @@ export default class CreateExercise extends Component {
         <div className="form-group">
           <label>Duration (in minutes): </label>
           <input 
-              type="text" 
+              type="number" 
               className="form-control"
               value={this.state.duration}
               onChange={this.onChangeDuration}
