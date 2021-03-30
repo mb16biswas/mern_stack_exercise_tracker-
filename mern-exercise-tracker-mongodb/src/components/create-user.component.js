694f92bdb75358b10e54b9bb351205c602067a7c
mb16biswas <64213233+mb16biswas@users.noreply.github.com>
2021-03-30 12:47:58 +0530

    Update create-user.component.js

--- mern-exercise-tracker-mongodb/src/components/create-user.component.js
+++ mern-exercise-tracker-mongodb/src/components/create-user.component.js
@@ -28,7 +28,7 @@ export default class CreateUser extends Component {
 
     console.log(user);
 
-    axios.post('https://gym-deploy-safe.herokuapp.com/users/add', user)
+    axios.post('http://localhost:5000/users/add', user)
       .then(res => console.log(res.data));
 
     this.setState({
